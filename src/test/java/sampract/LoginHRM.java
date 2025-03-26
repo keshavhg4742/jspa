b5aadd7a78c926f99cee4e05dd47091a1c2fc6fa
DELL <keshavhg4742.com>
2025-03-26 16:35:27 +0530

    com

---
+++ src/test/java/sampract/LoginHRM.java
@@ -0,0 +1,22 @@
+package sampract;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginHRM {
+	@Test
+	public void login() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+	}
+
+}
